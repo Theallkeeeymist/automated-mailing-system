3e060534ff7d6de2390ba13467441bc4bc6a5f2c
Sudhanshu Anand <sudhanshu.anand1143@gmail.com>
2025-06-18 16:05:53 +0530

    Update Tabular data.sql

--- Tabular data.sql
+++ Tabular data.sql
@@ -1,30 +1,12 @@
 -- insert data into Customer table
 insert into Customer(customer_name, email_id)
 values
-     ('Sudhanshu Anand','sudhanshu.anand1143@gmail.com'),
-     ('Ashu','ashu.anand2929@gmail.com'),
-     ('Sudhanshu','sudhanshuanand4529@gmail.com'),
-     ('Akash Anand','akash.anand0298@gmail.com'),
-     ('Beekshan Chaki','beekshan.chaki@idbiintech.com'),
-     ('Anand','allkeeeymist@gmai.com'),
-     ('Shreyanshu Raj','me.shreyanshuraj@gmail.com'),
-     ('Sudhanshu A','btech10342.23@bitmesra.ac.in'),
-     ('Abhinav Kant','btech10323.23@bitmesra.ac.in'),
-     ('Deepika','btech10153.23@bitmesra.ac.in'),
-     ('Aryan Jha', 'btech10344.23@bitmesra.ac.in');
+     ('NAME','EMAIL'),
+    
      
 -- insert data into credit cart table
 insert into Credit_card(customer_id, credit_card_number, flag)
 values
      ('1', '1246 2455 2222 1432', 'Y'),
-     ('2', '5544 8266 8749 8921', 'Y'),
-     ('3', '1235 6987 7542 0125', 'Y'),
-     ('4', '9780 8542 8566 2243', 'N'),
-     ('5', '7769 5962 2563 2144', 'Y'),
-     ('6', '0000 0000 0000 0000', 'N'),
-     ('7', '7895 6321 2245 1123', 'Y'),
-     ('8', '6661 3567 4452 4200', 'N'),
-     ('9', '9685 6321 2103 5014', 'N'),
-     ('10', '2025 2004 5006 5241', 'N'),
-     ('11', '1111 1111 1111 1111', 'N');
+     
      
